a62bb6c9527d3e6fd4cd3f0db30e6e168fe2a229
linwenjun <linwenjun@163.com>
2020-09-14 07:48:56 +0000

    first commit

---
+++ src/main/java/com/thoughtworks/cleanarch/CleanArchApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.cleanarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thoughtworks/cleanarch/CleanArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.cleanarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
